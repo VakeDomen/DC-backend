57a43bb7e1dd387b291c5c4ca8f8aa2fa61281ac
Vake Domen <vake.domen94@gmail.com>
2019-10-24 21:05:39 +0200

    added database migration

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # DC-backend
+
+## DB 
+
+### dependencies
+sudo apt-get install libsqlite3-dev
+cargo install diesel_cli --no-default-features --features "sqlite"
+
+### install
+diesel migration run
+
+## Auth dependencies
+sudo apt-get install clang llvm-dev libclang-dev
+
+## Run
+cargo run
---
+++ migrations/2019-10-24-184929_initialize/up.sql
@@ -0,0 +1,17 @@
+-- Your SQL goes here
+create table users
+(
+    id          varchar not null primary key,
+    name        varchar not null,
+    password    varchar not null,
+    active      varchar not null
+);
+
+create table notes
+(
+    id          varchar not null primary key,
+    user_id     varchar not null,
+    title       varchar not null,
+    date_tag    datetime not null,
+    body        varchar not null
+);
